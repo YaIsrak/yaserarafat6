307a9656ca625bbf4482888989162ab882879f3f
YaIsrak <61416887+YaIsrak@users.noreply.github.com>
2022-08-20 15:42:47 +0600

    done

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,7 @@
 export default function Blog({ id, title, content, date }) {
 	return (
-		<div
-			className='col-md-6 my-3 card border-none bg-indigo-100/0 text-white w-1/2'
-			id={id}>
-			<div className='px-5 card-body bg-indigo-500'>
+		<div className='my-3 card border-none bg-indigo-100/0 text-white' id={id}>
+			<div className='px-4 card-body bg-indigo-500'>
 				<h2 className='h2 card-title pt-4'> {title} </h2>
 				<p className='text-indigo-300 font2'>
 					{new Date(date).toLocaleString()}
@@ -12,7 +10,10 @@ export default function Blog({ id, title, content, date }) {
 				<p className='text-indigo-300 font2 fs-5'>
 					{`// This note was imported from Firebase`}
 				</p>
-				<p className='pb-4'> {content} </p>
+				<p className='lh-base pb-4' style={{ textAlign: 'justify' }}>
+					{' '}
+					{content}{' '}
+				</p>
 			</div>
 		</div>
 	);
