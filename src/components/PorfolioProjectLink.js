91cacb58e73a9e4857decaed981d2401e62b830e
YaIsrak <61416887+YaIsrak@users.noreply.github.com>
2022-08-20 14:57:43 +0600

    all good

--- src/components/PorfolioProjectLink.js
+++
@@ -1,9 +0,0 @@
-export default function PorfolioProjectLink({ name, link }) {
-	return (
-		<li>
-			<a href={link} className='link-light'>
-				{name}
-			</a>
-		</li>
-	);
-}
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,11 +1,13 @@
 import '../../scss/custom.css';
 import PortfolioSec1 from '../PorfolioSec1';
 import PorfolioSec2 from '../PorfolioSec2';
+import PortfolioSec3 from '../PortfolioSec3';
 
 export default function Portfolio() {
 	return (
 		<>
 			<PortfolioSec1 />
+			<PortfolioSec3 />
 			<PorfolioSec2 />
 		</>
 	);
