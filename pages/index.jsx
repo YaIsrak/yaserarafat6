09df655bf4cf708c619dcd73d12f9ab43ade6b49
YaIsrak <61416887+YaIsrak@users.noreply.github.com>
2022-09-14 20:05:46 +0600

    some changed

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,8 @@
 import Head from 'next/head';
 import Layout from '../components/Layout';
-import PortfolioSec1 from './components/portfoliosec1';
+import PortfolioSec1 from '../components/PorfolioSec1';
 import PortfolioSec3 from '../components/PortfolioSec3';
-import HeroSec from './components/herosec';
+import HeroSec from '../components/HomeSec1';
 
 export default function Home() {
 	return (
